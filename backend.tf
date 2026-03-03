7853ef19382c45edfb680016bcf3503f0d1c55a1
Ben-Chang-Systex <ben_chang@xsolutions.com.tw>
2026-03-03 11:21:41 +0800

    tyr3

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "gcs" {
     # 填寫剛剛建立的 Bucket 名稱 (不需要 gs:// 前綴)
-    bucket  = "ben-test-terraform-state-bucket"
-    
+    bucket = "ben-test-terraform-state-bucket"
+
     # Prefix 相當於資料夾路徑。
     # 這能讓同一個 Bucket 存放多個不同專案的狀態檔，這裡我們設定為 gke-cluster
-    prefix  = "terraform/gke/state"
+    prefix = "terraform/gke/state"
   }
 }
